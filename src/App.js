c6b3b3c6cddb4c2a28204e24ce96e94b30c75c35
Soham Datta <sohamdatta34@gmail.com>
2022-12-05 10:51:41 +0530

    1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Name: <strong>Soham Datta</strong> <br></br>
+          Enrollment Number: E21CSEU0980
         </p>
         <a
           className="App-link"
