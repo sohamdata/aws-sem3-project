224a330a3f2c28886915fb97617ca27f3fc57815
Soham Datta <sohamdatta34@gmail.com>
2022-12-05 11:47:05 +0530

    added auth and setup amplifyRole

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "awssem3projecta58bc7f9": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
